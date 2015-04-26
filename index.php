7eebe28bd1410b275832d5f5936b70646fa024f5
masterShake <vpgiannini@yahoo.com>
2015-04-26 11:40:08 -0700

    first commit. Added a navbar to the index.

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Routesider</title>
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Routesider styles -->
+        <link href="css/routesider.css" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+
+        <!-- navbar -->
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Brand</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Map <span class="sr-only">(current)</span></a></li>
+                        <li><a href="#">Deals</a></li>
+                        <li><a href="#">Sign Up</a></li>
+                    </ul>
+                    <form class="navbar-form navbar-left" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Search">
+                        </div>
+                        <button type="submit" class="btn btn-default">Submit</button>
+                    </form>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                <span>^_^</span>
+                                <span>username@email.com</span> 
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="#">username</a></li>
+                                <li><a href="#">my collectives</a></li>
+                                <li><a href="#">settings</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">edit profile</a></li>
+                                <li><a href="#">edit products</a></li>
+                                <li><a href="#">edit deals</a></li>
+                                <li><a href="#">edit map</a></li>
+                                <li><a href="#">edit social media</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">log out</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+
+        <!-- jumbotron -->
+        <div class="jumbotron">
+            <div class="container">
+
+                <div class="row">
+
+                    <!-- logo & tagline -->
+
+                    <!-- lead & register link -->
+
+                </div>
+
+                <!-- local featured products, deals, companies -->
+
+            </div>
+        </div>
+
+        <!-- main -->
+        <div class="container" role="main">
+
+            <!-- search & map -->
+
+            <!-- register & about -->
+
+        </div>
+
+    </body>
+</html>
