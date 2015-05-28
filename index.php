b2f4b629c0c394701af6e3f6a784f382b7d8ed58
masterShake <vpgiannini@yahoo.com>
2015-05-28 10:30:56 -0700

    added a slideout navbar menu element and some css to assist with the animation. Next step is to create a javascript function to manipulate it.

--- index.php
+++ index.php
@@ -57,129 +57,139 @@
     </head>
     <body>
 
+        <!-- page content -->
+        <div id="page-content">
+
+            <!-- navbar -->
+            <nav class="navbar navbar-map">
+                <div class="container">
+                    <div class="navbar-header">
+                        
+                        <!-- toggle mobile menu -->
+                        <button type="button" 
+                                class="navbar-toggle collapsed"
+                                id="navbar-toggle-menu"
+                                aria-describedby="Toggle navigation menu">
+                            <span class="glyphicon glyphicon-menu-hamburger"></span>
+                        </button>
+
+                        <!-- routesider logo -->
+                        <a class="navbar-brand" href="#">
+                            <div class="logo">
+                                <div>Route</div>
+                                <span>sider</span>
+                            </div>
+                        </a>
+
+                        <!-- search routesider -->
+                        <button type="button"
+                                style="float:right;"
+                                class="navbar-toggle"
+                                id="navbar-toggle-search"
+                                aria-describedby="Click to display search bar.">
+                            <span class="glyphicon glyphicon-search"></span>
+                        </button>
+
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#">Map <span class="sr-only">(current)</span></a></li>
+                            <li><a href="#">Deals</a></li>
+                            <li><a href="#">Sign Up</a></li>
+                        </ul>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    <span class="user-pic"><span class="glyphicon glyphicon-user"></span></span>
+                                    <!-- <span class="visible-md visible-lg">username@email.com</span>  -->
+                                    <span class="caret"></span>
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">username</a></li>
+                                    <li><a href="#">my collectives</a></li>
+                                    <li><a href="#">settings</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">edit profile</a></li>
+                                    <li><a href="#">edit products</a></li>
+                                    <li><a href="#">edit deals</a></li>
+                                    <li><a href="#">edit map</a></li>
+                                    <li><a href="#">edit social media</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">log out</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form class="navbar-form navbar-right" role="search">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="products, businesses, locations, etc...">
+                                <span class="input-group-btn">
+                                    <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search"></span></button>
+                                </span>
+                            </div><!-- /input-group -->
+                        </form>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
 
 
-        <!-- navbar -->
-        <nav class="navbar navbar-map">
-            <div class="container">
-                <div class="navbar-header">
-                    
-                    <!-- toggle mobile menu -->
-                    <button type="button" 
-                            class="navbar-toggle collapsed"
-                            id="navbar-toggle-menu"
-                            aria-describedby="Toggle navigation menu">
-                        <span class="glyphicon glyphicon-menu-hamburger"></span>
-                    </button>
+            <!-- map key=AIzaSyCCV74W4fbd9w1PVxD-OviILs9MPFqFdis -->
+            <div id="map-canvas"></div>
 
-                    <!-- routesider logo -->
-                    <a class="navbar-brand" href="#">
-                        <div class="logo">
-                            <div>Route</div>
-                            <span>sider</span>
-                        </div>
-                    </a>
-
-                    <!-- search routesider -->
-                    <button type="button"
-                            style="float:right;"
-                            class="navbar-toggle"
-                            id="navbar-toggle-search"
-                            aria-describedby="Click to display search bar.">
-                        <span class="glyphicon glyphicon-search"></span>
-                    </button>
-
-                </div>
-
-                <!-- Collect the nav links, forms, and other content for toggling -->
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="#">Map <span class="sr-only">(current)</span></a></li>
-                        <li><a href="#">Deals</a></li>
-                        <li><a href="#">Sign Up</a></li>
-                    </ul>
-                    <ul class="nav navbar-nav navbar-right">
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                <span class="user-pic"><span class="glyphicon glyphicon-user"></span></span>
-                                <!-- <span class="visible-md visible-lg">username@email.com</span>  -->
-                                <span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#">username</a></li>
-                                <li><a href="#">my collectives</a></li>
-                                <li><a href="#">settings</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">edit profile</a></li>
-                                <li><a href="#">edit products</a></li>
-                                <li><a href="#">edit deals</a></li>
-                                <li><a href="#">edit map</a></li>
-                                <li><a href="#">edit social media</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#">log out</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <form class="navbar-form navbar-right" role="search">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="products, businesses, locations, etc...">
-                            <span class="input-group-btn">
-                                <button class="btn btn-default" type="submit"><span class="glyphicon glyphicon-search"></span></button>
-                            </span>
-                        </div><!-- /input-group -->
-                    </form>
-                </div><!-- /.navbar-collapse -->
-            </div><!-- /.container-fluid -->
-        </nav>
-
-
-        <!-- map -->
-        <!-- key=AIzaSyCCV74W4fbd9w1PVxD-OviILs9MPFqFdis" -->
-        <div id="map-canvas"></div>
-
-        
-        <!-- jumbotron -->
-        <div class="container jumbo-container" id="jumbo-container">
-                
-            <!-- pull tab -->
-            <ul class="nav nav-tabs" 
-                id="jumbo-pull-tab"
-                role="tablist">
-                <li class="active" role="presentation">
-                    <a href="#" 
-                       class="map-pull-tab"
-                       role="tab">
-                        <span class="glyphicon glyphicon-chevron-up"></span>
-                    </a>
-                </li>
-            </ul>
-
-            <section class="jumbotron map-overlay-s">
-
-                <!-- lead -->
-                <div class="row">                
-                    <article class="col-sm-8 col-md-6 jumbo-lead">
-                        <h1 class="logo">
-                            <div>Route</div>
-                            <span>sider</span>
-                        </h1>
-                        <p>Local Products &amp; Services</p>
-                    </article>
-                </div>
-
-                <!-- hook -->
-                <div class="row">
-                    <article class="col-sm-8 col-md-6 jumbo-hook">
-                        <h2>Showcase Your Business</h2>
-                        <p>Listing your products on Routesider can help you garner the online attention that you deserve.</p>
-                        <p><a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a></p>
-                        <p>(It's free!)</p>
-                    </article>
-                </div>
             
-            </section>
-        </div>
+            <!-- jumbotron -->
+            <div class="container jumbo-container" id="jumbo-container">
+                    
+                <!-- pull tab -->
+                <ul class="nav nav-tabs" 
+                    id="jumbo-pull-tab"
+                    role="tablist">
+                    <li class="active" role="presentation">
+                        <a href="#" 
+                           class="map-pull-tab"
+                           role="tab">
+                            <span class="glyphicon glyphicon-chevron-up"></span>
+                        </a>
+                    </li>
+                </ul>
 
+                <section class="jumbotron map-overlay-s">
+
+                    <!-- lead -->
+                    <div class="row">                
+                        <article class="col-sm-8 col-md-6 jumbo-lead">
+                            <h1 class="logo">
+                                <div>Route</div>
+                                <span>sider</span>
+                            </h1>
+                            <p>Local Products &amp; Services</p>
+                        </article>
+                    </div>
+
+                    <!-- hook -->
+                    <div class="row">
+                        <article class="col-sm-8 col-md-6 jumbo-hook">
+                            <h2>Showcase Your Business</h2>
+                            <p>Listing your products on Routesider can help you garner the online attention that you deserve.</p>
+                            <p><a class="btn btn-primary btn-lg" href="#" role="button">Sign Up</a></p>
+                            <p>(It's free!)</p>
+                        </article>
+                    </div>
+                
+                </section>
+            </div>
+
+        </div><!-- end page content -->
+
+
+        <!-- mobile slideout menu -->
+        <div id="slideout-menu">
+            <div class="logo">
+                <div>Route</div>
+                <span>sider</span>
+            </div>
+        </div>
 
         <!-- javascripts -->
         <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&amp;callback=rsApp.setUserLocation"></script>
