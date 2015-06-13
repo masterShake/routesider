a7e7409b42c0b290cc64c0f3e1fc6055bb353fa1
masterShake <vpgiannini@yahoo.com>
2015-06-13 09:34:04 -0700

    added some pages for the profile.

---
+++ js/edit_profile.js
@@ -0,0 +1,41 @@
+
+//--------------------------------------
+//
+//		   edit_profile app
+//
+//--------------------------------------
+
+//
+// - This file contains all the rs
+//   methods that can be found 
+//   specifically on edit_profile.php
+//
+// - functions are added as prototypes
+//   of the rs object.
+//
+
+
+
+/* edit_profile methods */
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* initialize */
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    // create new rs object
+    rsApp = new rs();
+
+}, false);
+
