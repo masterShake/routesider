e07a14c528dc1410431204945cbc8a81e07c1e31
masterShake <vpgiannini@yahoo.com>
2015-06-30 10:12:29 -0700

    created the necessary pages t edit products, added some framework code to the php page.

--- edit_products.php
+++ edit_products.php
@@ -1,120 +1,20 @@
 <?php
 
-
-
     //---------------------------
     // STEP 1: load the init file
     //---------------------------
 
-    require_once 'core/init.php';
+    require_once 'core/init.php';    
 
 
-    //---------------------------------------
-    // Don't need to worry about tokens here.
-    //
-    // Image only goes to the uploads folder,
-    // and is assigned to the user when they
-    // click save.
-
-    // handle form input
-    $fn = (isset($_SERVER['HTTP_X_FILE_NAME']) ? $_SERVER['HTTP_X_FILE_NAME'] : false);
-
-    if ($fn) {
-
-        $user = new User();
-
-        //-------------------------------------------
-        // - getfile information
-        // - $ff[0] = random number
-        // - $ff[1] = file extension
-        // - $ff[2] = img element type (banner or avatar)
-        $ff = explode(".", $fn);
-
-        $uFolder = $_SERVER["DOCUMENT_ROOT"]."/routesider/uploads/";
-
-        // AJAX call
-        file_put_contents(
-            $uFolder . $ff[0].'.'.$ff[1],
-            file_get_contents('php://input')
-        );
-
-        //-------------------------------------------
-        // - create a unique name for the file
-        // - e.g. "username_123.jpg";
-        $newName = rand(0, 1000);
-        $newName = $user->data("username") . "_" . $newName . "." . $ff[1]; 
-
-        // change the name of the file to ensure it is unique
-        rename( $uFolder . $ff[0].'.'.$ff[1], $uFolder . $newName ); //change this
-    
-        $json = [ "filename" => $newName, "imgType" => $ff[2], "token" => Token::generate() ];
-
-        $json = json_encode($json);
-
-        exit( $json );
-    }
-    // else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-
-    //  // form submit
-    //  $files = $_FILES['fileselect'];
-
-    //  foreach ($files['error'] as $id => $err) {
-    //      if ($err == UPLOAD_ERR_OK) {
-    //          $fn = $files['name'][$id];
-    //          move_uploaded_file(
-    //              $files['tmp_name'][$id],
-    //              '../../uploads/' . $fn
-    //          );
-    //          echo "<p>File $fn uploaded.</p>";
-    //      }
-    //  }
-    // }
-
-    
     //-------------------------------
     // STEP 3: handle GET/POST params
     //-------------------------------
 
     if(Input::exists()){
 
-        // check token
-        if( Token::check( Input::get("token") ) ){
+    	// do something
 
-            // set the user
-            $user = new User();
-
-            // create an empty errors array
-            $errors = [];
-
-            // set the business
-            $business = $user->business();
-            $business = $business[0];
-
-            // set the profile
-            $profile = $business->profile();
-
-            $profile->update( Input::get("vals") );
-
-            // return the json object
-            $json = [ 
-                        "errors" => $errors,
-                        "token"  => Token::generate() 
-                    ];
-
-            echo json_encode($json);
-
-        }else{
-
-            $json = [ 
-                        "errors" => [ "Token Timeout" ],
-                        "token"  => Token::generate()
-                    ];
-
-            echo json_encode( $json );
-
-        }
-
-        exit();
     }
 
 
@@ -122,7 +22,7 @@
     // instantiate global variables
     //-------------------------------------
 
-    $page = "edit_profile"; //required
+    $page = "edit_products";
 
     $errors = []; //required
 
@@ -131,8 +31,6 @@
     $business = $user->business();
 
     $business = $business[0];
-
-    $profile = $business->profile();
 
     //---------------------------------------
     // - if the user is not logged in, 
@@ -171,7 +69,7 @@
     </head>
     <body>
 
-        <!-- page content -->
+    	<!-- page content -->
         <div id="page-content">
 
             <!-- navbar -->
@@ -195,13 +93,28 @@
                             </span>
                         </a>
 
-                        <!-- search routesider -->
-                        <button type="button"
-                                style="float:right;"
-                                class="navbar-toggle"
-                                id="navbar-toggle-search">
-                            <span class="glyphicon glyphicon-search"></span>
-                        </button>
+                      	<ul class="navbar-right user-nav">
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    <span class="user-pic"><span class="glyphicon glyphicon-user"></span></span>
+                                    <!-- <span class="visible-md visible-lg">username@email.com</span>  -->
+                                    <span class="caret"></span>
+                                </a>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li><a href="#">username</a></li>
+                                    <li><a href="#">my collectives</a></li>
+                                    <li><a href="#">settings</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">edit profile</a></li>
+                                    <li><a href="#">edit products</a></li>
+                                    <li><a href="#">edit deals</a></li>
+                                    <li><a href="#">edit map</a></li>
+                                    <li><a href="#">edit social media</a></li>
+                                    <li class="divider"></li>
+                                    <li><a href="#">log out</a></li>
+                                </ul>
+                            </li>
+                        </ul>
 
                     </div>
 
@@ -246,152 +159,7 @@
                 </div>
             </nav>
 
-            <section class="container">
-
-                <!-- initial values -->
-                <input type="hidden" 
-                       value='<?= json_encode( $profile->data() ); ?>' 
-                       name="initvals" 
-                       id="initial-values">
-
-                <!-- token -->
-                <input type="hidden" 
-                       value='<?= Token::generate(); ?>' 
-                       name="token" 
-                       id="token">
-
-                <h1>Edit Profile</h1>
-
-                <!-- save button + activate/deactivate profile switch -->
-                <div class="alert alert-info" 
-                     role="alert" 
-                     style="display:none;" 
-                     id="save-alert1">
-                    <span>Click the <strong>save</strong> button to keep your changes.</span>
-                </div>
-
-                <div style="height: 80px;">
-
-                    <button class="btn" id="save-btn1" style="margin-top:40px;" aria-label="save changes">Save</button>
-
-                    <div style="float:right;">
-                        <h4>Page Visible</h4>
-                        <div class="onoffswitch" 
-                                   data-propname="active">
-                            <input type="checkbox" 
-                                   name="onoffswitch" 
-                                   class="onoffswitch-checkbox" 
-                                   id="myonoffswitch-profile"
-                                   <?= $profile->data("active") ? "checked" : ""; ?>>
-                            <label class="onoffswitch-label" for="myonoffswitch-profile">
-                                <span class="onoffswitch-inner"></span>
-                                <span class="onoffswitch-switch"></span>
-                            </label>
-                        </div>
-                    </div>
-                </div>
-
-                <hr>
-
-                <h4 style="margin-top:25px;">Edit Avatar &amp; Profile Banner</h4>
-                
-                <!-- edit banner -->
-                <form id="edit-banner" 
-                      style="margin-top:20px;"
-                      action="" 
-                      method="POST" 
-                      enctype="multipart/form-data">
-
-                    <div id="banner-filedrag" 
-                         data-elem="banner"
-                         style='background-image: <?= $profile->data("banner") ? "url(img/business/" . $profile->data("banner") . ");" : "none;" ?>' >or drop files here</div>
-
-                    <div class="traditional-upload">
-                        <label for="banner-fileselect">Files to upload:</label>
-                        <input type="file" 
-                               id="banner-fileselect" 
-                               name="fileselect[]" 
-                               multiple="multiple" />
-                    </div>
-                </form>
-
-                <!-- edit avatar -->
-                <form id="edit-avatar"  
-                      action="index.html" 
-                      method="POST" 
-                      enctype="multipart/form-data">
-
-                    <div id="avatar-filedrag" 
-                         data-elem="avatar" 
-                         style='background-image: <?= $profile->data("avatar") ? "url(img/business/" . $profile->data("avatar") . ");" : "none;" ?><?= $profile->data("avatar_shape") == "square" ? "border-radius: 0%;" : "" ?>' >or drop files here</div>
-
-                    <div id="semi-circle-traditional-upload"
-                         style='<?= $profile->data("avatar_shape") == "square" ? "border-top-left-radius: 0px; border-top-right-radius: 0px;" : "" ?>'>
-                        <label for="avatar-fileselect">Files to upload:</label>
-                        <input type="file" 
-                               id="avatar-fileselect" 
-                               name="fileselect[]" 
-                               multiple="multiple" />
-                    </div>
-                </form>
-                
-                <div class="avatar-shape-btns">
-                    <div style="float:left;">
-                        <div class="avatar-shape" style="border-radius:50%;"></div>
-                        <input type="radio" id="circle-avatar" name="avatar-shape" value="circle" checked>
-                    </div>
-                    <div style="float:right;">
-                        <div class="avatar-shape"></div>
-                        <input type="radio" id="square-avatar" name="avatar-shape" value="square">
-                    </div>
-                </div>
-
-                <hr>
-
-                <!-- business name -->
-                <div class="row">
-                    <div class="col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
-                        <h4>Business Name</h4>
-                        <input type="text" 
-                               name="name" 
-                               class="form-control" 
-                               id="business-name"
-                               value="<?= $profile->data("name") ?>">
-                    </div>
-                </div>
-
-                <!-- tagline -->
-                <div class="row">
-                    <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
-                        <h4>Tagline</h4>
-                        <input type="text" 
-                               name="tagline" 
-                               class="form-control" 
-                               id="tagline"
-                               value="<?= $profile->data("tagline") ?>">
-                    </div>
-                </div>
-
-                <!-- description -->
-                <div class="row">
-                    <div class="col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
-                        <h4>Description</h4>
-                        <textarea class="form-control" id="description" name="description"><?= $profile->data("description"); ?></textarea>
-                    </div>
-                </div>
-
-                <hr>
-
-                <div class="alert alert-info" 
-                     role="alert" 
-                     style="display:none;" 
-                     id="save-alert2">
-                    <span>Click the <strong>save</strong> button to keep your changes.</span>
-                </div>
-
-                <button class="btn" id="save-btn2" style="margin:20px;margin-bottom:38px;" aria-label="save changes">Save</button>
-
-            </section>
+            <h1>Edit Products</h1>
 
             <!-- footer -->
             <footer>
@@ -484,7 +252,7 @@
                 </div>
             </footer>
 
-        </div>
+        </div><!-- end page content -->
 
         <!-- contentent cover to close menu -->
         <div id="content-cover"></div>
@@ -559,3 +327,16 @@
 
     </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/edit_products.js
@@ -0,0 +1,36 @@
+
+//-----------------------------------------------
+//
+//		   		 edit_maps app
+//			   -----------------
+//
+// - This file contains all the methods that can
+//   be found specifically on edit_products.php
+//
+//-----------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
