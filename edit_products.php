d9963d37a37488bbd2f18422b35cf7efe556b952
masterShake <vpgiannini@yahoo.com>
2015-07-01 20:56:15 -0700

    added search field to edit_products.php. Stylized said search field.

--- edit_products.php
+++ edit_products.php
@@ -67,98 +67,36 @@
             <!-- navbar -->
             <?php include "components/navbar.php"; ?>
 
-            <h1>Edit Products</h1>
+            <div class="container">
+
+            	<!-- header -->
+            	<h1>Edit Products</h1>
+
+            	<!-- search products -->
+            	<div class="row">
+            		<div class="col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+						<div class="input-group input-group-lg search-products-input">
+							<input type="text" 
+								   class="form-control" 
+								   id="search-products"
+								   placeholder="Search products"
+								   aria-label="search products">
+							<button type="button" 
+									class="btn btn-lg" 
+									id="search-products-submit"
+									aria-label="Submit search for products">
+								<span class="glyphicon glyphicon-search"></span>
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!-- my products swiper -->
+
 
             <!-- footer -->
-            <footer>
-                <div class="container">
-                    <div class="row">
-
-                        <!-- quick links -->
-                        <div class="col-sm-4">
-                            
-                            <div class="logo">
-                                <a href="/">
-                                    <div class="route">Route</div>
-                                    <span>sider</span>
-                                </a>
-                            </div>
-
-                            <ul class="list-group">
-                                <li class="list-group-item">
-                                    <a href="about">About</a>
-                                </li>
-                                <li class="list-group-item">
-                                    <a href="register">Sign Up</a>
-                                </li>
-                                <li class="list-group-item">
-                                    <a href="privacy">Privacy</a>
-                                </li>
-                                <li class="list-group-item">
-                                    <a href="terms_of_use">Terms of Use</a>
-                                </li>
-                            </ul>
-
-                            <hr class="visible-xs">
-
-                        </div>
-
-                        <!-- contact info -->
-                        <div class="col-sm-4 contact-info">
-                            
-                            <div>
-                                <a href="tel:+6506781777">
-                                    <span class="glyphicon glyphicon-earphone"></span>
-                                </a>
-                            </div>
-                            <div style="font-size:10px;">
-                                <a href="tel:+6506781777">phone</a>
-                            </div>
-                            <div style="margin-bottom: 30px;">
-                                <a href="tel:+6506781777">(650) 678-1777</a>
-                            </div>
-
-                            <div class="glyphicon glyphicon-time"></div>
-                            <div style="font-size:10px;">phone hours</div>
-                            <div>10 - 10 <span style="font-size:8px;">(Silicon Valley)</span></div>
-                            <div>everyday</div>
-
-                            <hr class="visible-xs">
-
-                        </div>
-
-                        <!-- contact form -->
-                        <div class="col-sm-4 contact-us">
-
-                            <div class="glyphicon glyphicon-envelope"></div>
-                            <div style="font-size:10px;">contact us</div>
-
-                            <form>
-                                <input type="text" 
-                                       name="cemail" 
-                                       class="form-control"
-                                       id="cname" 
-                                       placeholder="your@email.com">
-                              
-                                <textarea id="cmessage" 
-                                          class="form-control" 
-                                          placeholder="500 characters max"
-                                          rows="5"></textarea>
-                              
-                                <div style="text-align:right;">
-                                    <button type="button" class="btn btn-default" aria-label="send message">Send</button>
-                                </div>
-
-                            </form>
-                            
-                        </div>
-
-                        <!-- copyright -->
-                        <div style="text-align:center;padding:30px;font-size: 12px;color: #aaa;">&#169; Routesider 2015</div>
-
-                    </div>
-                </div>
-            </footer>
+            <?= include "components/footer.php"; ?>
 
         </div><!-- end page content -->
 
