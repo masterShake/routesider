cccb2b1ebfc80309fa85b4f214713464d42d5ae1
masterShake <vpgiannini@yahoo.com>
2015-07-22 14:41:29 -0700

    finished half of the Markdown php class.

---
+++ classes/Markdown.php
@@ -0,0 +1,31 @@
+<?php
+
+/*-----------------------------------------------
+
+					Markdown
+				  ------------
+
+ - replace the markdown expressions with HTML
+   tags.
+
+-----------------------------------------------*/
+
+class Markdown{	
+	
+	//-----------------------------------------------
+	// - convert markdown to HTML
+	// - @ text -> text to be converted
+	// - @ toolbar -> "half" or "full"
+	// - returns html formatted string
+	public static function html( $text = "", $toolbar = "half" ){
+
+		// bold
+		$text = 
+
+	}
+
+	// set bold tags
+	private static function _bold( $text ){
+
+	}
+}
--- tests/t.php
+++ tests/t.php
@@ -7,16 +7,56 @@
 -----------------------------------------*/
 
 
+/*-----------------------------------------------
 
-// $x = preg_match("/#([a-f]|[A-F]|[0-9]){3}(([a-f]|[A-F]|[0-9]){3})?\b/", );
+					Markdown
+				  ------------
 
-// print_r($x);
+ - replace the markdown expressions with HTML
+   tags.
 
-if( 0 <= 1 && 1 <= 2 ){
+-----------------------------------------------*/
 
-	echo "operators gooood";
+class Markdown{	
+	
+	//-----------------------------------------------
+	// - convert markdown to HTML
+	// - @ text -> text to be converted
+	// - @ toolbar -> "half" or "full"
+	// - returns html formatted string
+	public static function html( $text = "", $toolbar = "half" ){
 
+		// bold
+		$text = preg_replace('/\*\*(.+?)\*\*/s', "<strong>$1</strong>", $text);
+
+		// italic
+		$text = preg_replace('/\*([^\*]+)\*/', "<i>$1</i>", $text);
+
+		// strikethrough
+		$text = preg_replace('/~~(.+?)~~/s', "<span style='text-decoration:line-through'>$1</span>", $text);
+
+		// subscript
+		$text = preg_replace('/\-\^\(([^\)]+)\)/', "<sub>$1</sub>", $text);
+
+		// superscript
+		$text = preg_replace('/\^\(([^\)]+)\)/', "<sup>$1</sup>", $text);
+
+		// if user is marking up textarea
+		if($toolbar == "full")
+
+			return self::fullHTML( $text );
+
+		return $text;
+
+	}
+
+	//-------------------------------------------
 }
+
+
+$myText = "***bitch* you *ain't* ~~shit~~**poop-^(2)!";
+
+echo Markdown::html( $myText );
 
 
 
