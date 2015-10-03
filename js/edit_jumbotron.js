1513ae9307582a96b2a1787839fb00569131f1fa
masterShake <vpgiannini@yahoo.com>
2015-10-03 13:50:23 -0700

    added some classes, methods, and general scaffolding to edit_jumbotron.js

--- js/edit_jumbotron.js
+++ js/edit_jumbotron.js
@@ -68,21 +68,36 @@ RS.prototype.toggleMobileMenu = function(){
 
 
 
+
+
+
+
+
+
+
+
+
+
 var Jumbo, jApp;
 
 (function(){
 
 	//-----------------------------------------------
-	//					Jumbo class				
-	//				  -------------
+	//				Jumbo (root node)			
+	//			  ---------------------
 	//
-	// - edit jumbotron (Jumbo)
+	// - set the canvas height
 	//
-	// - stylize jumbotron
+	// - initialize the various edit mode objects of
+	//   components toolbar
 	//
-	// - drag and drop text over jumbotron
+	// - keep track of/detect changes made
 	//
-	// - customize for mobile, tablet, & desktop
+	// - prompt save, save css
+	//
+	// - ajax call
+	//
+	// - save success message
 	//
 	//-----------------------------------------------
 
@@ -91,6 +106,17 @@ var Jumbo, jApp;
 	Jumbo = function(){ return false;
 
 		/* properties */
+
+		// keep track of initial values
+		this.iVals = JSON.parse( document.getElementById("i-vals").value );
+		// keep track of new values (identical at first)
+		this.nVals = JSON.parse( document.getElementById("i-vals").value );
+
+		// init background editor
+		this.bg = new BG();
+
+		// temp variable
+		this.temp;
 
 		/* initializations */
 
@@ -105,6 +131,199 @@ var Jumbo, jApp;
 	}
 
 	/* METHODS */
+
+	//-----------------------------------------------
+	// - determine if the user has made any changes
+	// - returns true if changed, false if unchanged
+	Jumbo.prototype.deltaVals = function(){
+		
+		// if the values have not changed
+		if( JSON.stringify(this.iVals) === JSON.stringify(this.nVals)){
+
+			// hide save alert
+			this.hideSA();
+
+			return false;
+		}
+
+		// display save prompt
+		this.showSA();
+
+		return true;
+
+	}
+
+	//-----------------------------------------------
+	// - show save alert
+	Jumbo.prototype.showSA = function(){
+		return false;
+	}
+
+	//-----------------------------------------------
+	// - hide save alert
+	Jumbo.prototype.hideSA = function(){
+		return false;
+	}
+
+	//-----------------------------------------------
+	// - ajax save changes
+	Jumbo.prototype.save = function(){
+
+		// indicate that save request is in progress
+
+		// make save ajax call
+
+	}
+
+	//-----------------------------------------------
+	// - save callback
+	Jumbo.prototype.saveCB = function(r){ console.log(r);
+
+		// indicate that save was successful
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//-----------------------------------------------
+	//				BG (edit background)				
+	//			  ------------------------
+	//
+	// - manage background options btn classes
+	//
+	// - toggle display background image
+	//
+	//-----------------------------------------------
+
+	/* CONSTRUCTOR */
+
+	BG = function(){ 
+
+		/* properties */
+
+		// init cropper
+		this.cropper = new CR();
+		// init background color editor
+		this.bgCol = new BGC();
+		// init background image editor
+		this.bgImg = new BGI();
+
+		/* initializations */
+
+		// toggle bg image display
+		document.getElementById("bg-opts-toolbar")
+			.children[0].children[0]
+				.addEventListener("change", this.toggleImg, false);
+
+		// add event listeners to btns in bg options toolbar
+
+	}
+
+	/* METHODS */
+
+	//-----------------------------------------------
+	// - apply or remove background image
+	// - change eyeball icon
+	// - prompt save
+	BG.prototype.toggleImg = function(){
+		return false;
+	}
+
+	//-----------------------------------------------
+	// - background options toolbar btn event
+	// - add active class to clicked btn
+	// - remove active class from any other btn
+	BG.prototype.actBtn = function(){
+
+		// if this button is already active
+		if( this.className.substr(this.className.length - 6) == "active"){
+			// remove active class
+			this.className = this.className.substr(0, this.className.length - 7);
+
+			return false;
+		}
+
+
+		// look for another active btn
+		jApp.temp = this.parentElement.getElementsByClassName("active");
+
+		// if there is another active btn
+		if(jApp.temp.length) 
+			// remove its active class
+			jApp.temp[0].className = jApp.temp[0].className
+										.substr(0, jApp.temp[0].className.length - 7);
+
+		// activate this btn
+		this.className = this.className + " active";
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
